6f9463e01dd67e544bb72ec55750677fec5e81a2
ciaran oriordan <ciaranoriordan93@gmail.com>
2019-07-19 00:52:56 +0100

    added projects

--- js/app.js
+++ js/app.js
@@ -2,6 +2,36 @@ const navBtn = document.getElementById("navi-toggle");
 const navMenu = document.querySelector(".dropdown");
 const carouselBtn = document.querySelectorAll(".client-button");
 const carouselContent = document.querySelectorAll(".testimonials__carousel");
+const workHandle = document.querySelector(".work");
+const modalHandle1 = document.querySelector(".modal__filmbase");
+const modalHandle2 = document.querySelector(".modal__portfolio");
+const modalHandle3 = document.querySelector(".modal__sytist");
+const modalClose = document.querySelectorAll(".modal__close");
+
+// display the corresponding modal to the appropriate clicked image element
+workHandle.addEventListener("click", event => {
+  if (event.target.nodeName === "IMG") {
+    if (
+      event.target.parentElement.parentElement.className === "work__filmbase"
+    ) {
+      modalHandle1.classList.toggle("show");
+    } else if (
+      event.target.parentElement.parentElement.className === "work__portfolio"
+    ) {
+      modalHandle2.classList.toggle("show");
+    } else if (
+      event.target.parentElement.parentElement.className === "work__sytist"
+    ) {
+      modalHandle3.classList.toggle("show");
+    }
+  }
+});
+
+for (let i = 0; i < modalClose.length; i++) {
+  modalClose[i].addEventListener("click", event => {
+    event.target.parentElement.parentElement.classList.toggle("show");
+  });
+}
 
 navBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show");
